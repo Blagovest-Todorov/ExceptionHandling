e59ee43b76ed5aca3df89baa87cb9ebfe47d3858
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-22 21:43:38 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.exceptionsHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //throw exception 
+            //expections is mechanism of handling errors, events;
+            try
+            {
+                int a = int.Parse("Wrong1");
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Please enter a number whole , number only as a input format"); ;
+            }
+            
+        }
+    }
+}
