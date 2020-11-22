a5b2114e262d9ceef52043aadcb15d54e67ef22a
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-22 21:59:51 +0200

    more ecxplanations for exception

--- Program.cs
+++ Program.cs
@@ -10,14 +10,15 @@ namespace _01.exceptionsHandling
             //expections is mechanism of handling errors, events;
             try
             {
-                int a = int.Parse("Wrong1");
+                int a = int.Parse(Console.ReadLine());
             }
             catch (Exception)
             {
 
                 Console.WriteLine("Please enter a number whole , number only as a input format"); ;
             }
-            
+            //exceptions are handeled with structures try{}-> catch(){}/
+
         }
     }
 }
